29935faf0f23be3c66714cf1a4e7145a23b85220
YuHRong <1791867903@qq.com>
2026-01-23 00:06:16 +0800

    fall

---
+++ Code/CBook/CPrimerPlus/BCode/L11/11.6 getsputs.c
@@ -0,0 +1,18 @@
+/*  getsputs.c  -- 使用 gets() 和 puts() */
+#include<stdio.h>
+#define STLEN 81
+
+int main(void)
+{
+ char words[STLEN];
+
+ puts("Enter a string, please");
+ gets(words);   // 典型用法
+
+ printf("Your string twice:\n");
+ printf("%s\n", words);
+ puts(words);
+ puts("Done.");
+
+ return 0;
+}
