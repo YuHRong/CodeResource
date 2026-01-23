c0346501c5affb3105442c4223cc7ff604036339
YuHRong <1791867903@qq.com>
2026-01-23 12:12:33 +0800

    重新整理目录，让每个语言都放在单独的目录中。

--- CLanguage/大小排序/min.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(void){
-    int a = 10, b = 20, c = 15;
-    int min = (a<b )?((a < c)? a : c):((b < c)? b : c);
-printf("The minimum value is: %d\n", min);
-    return 0;
-}
