4f2b92f87ae3f1cbca7dd36ac754b1a816bfb732
YuHRong <1791867903@qq.com>
2026-01-28 10:26:55 +0800

    ADD practice program

---
+++ C#/WinForm/FormsStudy/Practice/PT1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace PT1
+{
+ partial class Form1
+ {
+  /// <summary>
+  /// 必需的设计器变量。
+  /// </summary>
+  private System.ComponentModel.IContainer components = null;
+
+  /// <summary>
+  /// 清理所有正在使用的资源。
+  /// </summary>
+  /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+  protected override void Dispose(bool disposing)
+  {
+   if (disposing && (components != null))
+   {
+    components.Dispose();
+   }
+   base.Dispose(disposing);
+  }
+
+  #region Windows 窗体设计器生成的代码
+
+  /// <summary>
+  /// 设计器支持所需的方法 - 不要修改
+  /// 使用代码编辑器修改此方法的内容。
+  /// </summary>
+  private void InitializeComponent()
+  {
+   this.btnOpen = new System.Windows.Forms.Button();
+   this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+   this.listView1 = new System.Windows.Forms.ListView();
+   this.btnPlay = new System.Windows.Forms.Button();
+   this.btnStop = new System.Windows.Forms.Button();
+   this.SuspendLayout();
+   // 
+   // btnOpen
+   // 
+   this.btnOpen.AutoSize = true;
+   this.btnOpen.Location = new System.Drawing.Point(0, -6);
+   this.btnOpen.Name = "btnOpen";
+   this.btnOpen.Size = new System.Drawing.Size(75, 23);
+   this.btnOpen.TabIndex = 0;
+   this.btnOpen.Text = "打开文件";
+   this.btnOpen.UseVisualStyleBackColor = true;
+   this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+   // 
+   // openFileDialog1
+   // 
+   this.openFileDialog1.FileName = "openFileDialog1";
+   // 
+   // listView1
+   // 
+   this.listView1.HideSelection = false;
+   this.listView1.Location = new System.Drawing.Point(0, 0);
+   this.listView1.Name = "listView1";
+   this.listView1.Size = new System.Drawing.Size(121, 97);
+   this.listView1.TabIndex = 1;
+   this.listView1.UseCompatibleStateImageBehavior = false;
+   // 
+   // btnPlay
+   // 
+   this.btnPlay.Location = new System.Drawing.Point(0, 0);
+   this.btnPlay.Name = "btnPlay";
+   this.btnPlay.Size = new System.Drawing.Size(75, 23);
+   this.btnPlay.TabIndex = 2;
+   this.btnPlay.Text = "播放";
+   this.btnPlay.UseVisualStyleBackColor = true;
+   this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+   // 
+   // btnStop
+   // 
+   this.btnStop.Location = new System.Drawing.Point(8, 8);
+   this.btnStop.Name = "btnStop";
+   this.btnStop.Size = new System.Drawing.Size(75, 23);
+   this.btnStop.TabIndex = 3;
+   this.btnStop.Text = "停止";
+   this.btnStop.UseVisualStyleBackColor = true;
+   // 
+   // Form1
+   // 
+   this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+   this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+   this.ClientSize = new System.Drawing.Size(800, 450);
+   this.Controls.Add(this.btnStop);
+   this.Controls.Add(this.btnPlay);
+   this.Controls.Add(this.listView1);
+   this.Controls.Add(this.btnOpen);
+   this.Name = "Form1";
+   this.Text = "Form1";
+   this.ResumeLayout(false);
+   this.PerformLayout();
+
+  }
+
+  #endregion
+
+  private System.Windows.Forms.Button btnOpen;
+  private System.Windows.Forms.OpenFileDialog openFileDialog1;
+  private System.Windows.Forms.ListView listView1;
+  private System.Windows.Forms.Button btnPlay;
+  private System.Windows.Forms.Button btnStop;
+ }
+}
+
---
+++ C#/WinForm/FormsStudy/Practice/PT1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+
+namespace PT1
+{
+ public partial class Form1 : Form
+ {
+  public Form1()
+  {
+   InitializeComponent();
+  }
+
+  private void btnOpen_Click(object sender, EventArgs e)
+  {
+   openFileDialog1.Multiselect = true;
+   openFileDialog1.Title = "请选择文件";
+   openFileDialog1.Filter = "所有文件(*.*)|*.*";
+   if (openFileDialog1.ShowDialog() == DialogResult.OK)
+   {
+    listView1.Items.Clear();
+    listView1.View = View.List;
+
+    foreach(string fileName in openFileDialog1.FileNames)
+    {
+     listView1.Items.Add(Path.GetFileName(fileName));
+    }
+   }
+  }
+
+  private void btnPlay_Click(object sender, EventArgs e)
+  {
+   SoundPlayer player = new SoundPlayer();
+   SoundPlayer.ReferenceEquals() = File;
+  }
+ }
+}
