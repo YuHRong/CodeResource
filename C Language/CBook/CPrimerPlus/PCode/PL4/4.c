3df4b911abb463b2de784e9c0bbe26d7c990c1d8
YuHRong <1791867903@qq.com>
2026-02-04 07:23:03 +0800

    添加 第四章 编程练习 4

--- C Language/CBook/CPrimerPlus/PCode/PL4/4.c
+++ C Language/CBook/CPrimerPlus/PCode/PL4/4.c
@@ -3,5 +3,19 @@
 
 int main(void)
 {
+ float highCm;
+ float highInc;
+ char name[20];
 
+ printf("请输入你的升高，以英寸为单位:\n");
+ scanf("%f", &highInc);
+
+ printf("请输入你的身高，厘米为单位:\n");
+ scanf("%f", &highCm);
+
+ printf("请输入你的姓名:\n");
+ scanf("%s", name);
+ printf("%s 你的升高是: %.3f 英尺，你的升高是 %.3f 米\n", name, highInc / 12.0, highCm / 100.0);
+
+  return 0;
 }
