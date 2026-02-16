d4eb57366f9d0e936149599aa9740538c6d1b614
YuHRong <1791867903@qq.com>
2026-02-16 17:47:30 +0800

    新春快乐，写个练习。

--- C Language/CBook/CPrimerPlus/PCode/PL6/5.c
+++ C Language/CBook/CPrimerPlus/PCode/PL6/5.c
@@ -3,5 +3,32 @@
 
 int main(void)
 {
+ int i = 0;
+ char ch;
 
+ printf("请输入一个大写字母: ");
+ scanf("%c", &ch);
+
+ while (i++ < 5)
+ {
+  int j = 1, k = 5;
+  int c = ch;
+  while (k > c)
+  {
+   putchar(' ');    // 打印空格
+   k--;
+  }
+  while (j < i)
+  {
+   printf("%c", c++);
+   j++;
+  }
+  while (j > 0)
+  {
+   printf("%c", c--);
+   j--;
+  }
+  printf("\n");
+ }
+ return 0;
 }
