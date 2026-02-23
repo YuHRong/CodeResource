1585fd8c0878fef6df22258f36e80477150949b8
YuHRong <1791867903@qq.com>
2026-02-23 09:24:53 +0800

    添加python 循环学习

---
+++ Study/python/python 循环/使用循环和序列/01. 工作原理.py
@@ -0,0 +1,17 @@
+'''
+lists
+tuples
+ranges
+他们是python 中最基本的基本序列类型。
+'''
+##列表数据类型是由字串、数字甚至其他列表组成的数组元素序列。列表是可变的，使用零基索引，这意味着列表的第一个元素位于零基索引。
+
+## 基础语法： 
+
+cities = ['Los Angeles', 'London', 'Tokyo']
+## 访问 cities 中的第一个列表可以使用: 
+cities[0]
+print(cities[1])
+## 访问最后的列表元素可以使用 "减号"来进行访问，如： 
+cities[-1]
+print(cities[-1])
