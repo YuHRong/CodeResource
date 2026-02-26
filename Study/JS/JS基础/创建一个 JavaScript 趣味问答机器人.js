ba439684a0a102cd7b297c68e884f051b806cfae
YuHRong <1791867903@qq.com>
2026-02-26 19:50:50 +0800

    添加 JS 一个机器人小程序

---
+++ Study/JS/JS基础/创建一个 JavaScript 趣味问答机器人.js
@@ -0,0 +1,28 @@
+// 1. 输出问候信息
+console.log("Hello! I'm your coding fun fact guide!");
+
+// 2. 创建三个变量
+let botName = "CodeBot";
+let botLocation = "the Cloud";
+let favoriteLanguage = "JavaScript";
+
+// 3. 输出机器人基本信息
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+
+// 4. 输出最喜欢的编程语言
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+// 5. 创建codingFact变量并赋值第一个有趣事实
+let codingFact = `Did you know that ${favoriteLanguage} was created in just 10 days?`;
+console.log(codingFact);
+
+// 7. 重新赋值codingFact为第二个事实
+codingFact = `${favoriteLanguage} is the most popular programming language according to GitHub's annual reports.`;
+console.log(codingFact);
+
+// 9. 再次重新赋值codingFact为第三个事实
+codingFact = `One interesting fact about ${favoriteLanguage} is that it's not related to Java despite the name similarity!`;
+console.log(codingFact);
+
+// 11. 输出告别语句
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
