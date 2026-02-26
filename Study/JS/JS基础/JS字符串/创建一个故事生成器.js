e9e041c0ffc7aa95e69d5d675aa6796bb8e61773
YuHRong <1791867903@qq.com>
2026-02-26 22:44:00 +0800

    整理 JS 目录

---
+++ Study/JS/JS基础/JS字符串/创建一个故事生成器.js
@@ -0,0 +1,36 @@
+// 1. 你应使用 let 声明以下变量：
+let adjective, noun, verb, place, adjective2, noun2;
+
+// 2. 你应该将上述变量赋值为你所选择的字符串值。
+adjective = "fierce";
+noun = "dragon";
+verb = "roared";
+place = "mountain cave";
+adjective2 = "golden";
+noun2 = "treasure";
+
+// 3. 你应该声明一个 firstStory 变量。
+let firstStory;
+
+// 4. 你应使用以下故事模板创建第一个故事，并将其分配给 firstStory 变量：
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+// 5. 你应该将第一个故事输出到控制台，使用的信息是 "First story: [firstStory]"。
+console.log("First story: " + firstStory);
+
+// 6. 你应该为 adjective、noun、verb、place、adjective2 和 noun2 变量重新赋值。
+adjective = "silly";
+noun = "wizard";
+verb = "danced";
+place = "enchanted forest";
+adjective2 = "sparkling";
+noun2 = "wand";
+
+// 7. 你应该声明一个 secondStory 变量。
+let secondStory;
+
+// 8. 使用相同的模板创建另一个故事，并将其赋值给 secondStory 变量。
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+// 9. 你应该将第二个故事输出到控制台，使用的信息是 "Second story: [secondStory]"。
+console.log("Second story: " + secondStory);
