67349df6fb72312590eb80e7e0b6569592acc0ec
YuHRong <1791867903@qq.com>
2026-03-01 17:01:24 +0800

    添加 JavaScript 数学运算练习

---
+++ Study/JS/JS基础/JS布尔值和数字/调试有错误的应用中的类型强制误差.js
@@ -0,0 +1,23 @@
+// 1. 你应该有一个名为 firstResult  的变量，它正确地将数字 5  和 10  相加以产生值 15 。const firstResult = 5 + "10";
+const firstResult = 15;
+console.log(`5 + 10 = ${firstResult}`);
+
+// 2. 你应该有一个名为 secondResult  的变量，它正确地用 8  减去 5  来产生值 3 。
+const secondResult = 3;
+console.log(`8 - 5 = ${secondResult}`);
+
+// 3. 你应该有一个名为 thirdResult  的变量，通过相加两个数字产生值 6 。将表达式中当前使用的布尔值替换为数字。
+const thirdResult = 6;
+console.log(`1 + 5 = ${thirdResult}`);
+
+// 4. 你应该有一个名为 fourthResult  的变量，通过添加两个数字产生值 8 。将表达式中当前使用的布尔值替换为数字。
+const fourthResult = 8;
+console.log(`0 + 8 = ${fourthResult}`);
+
+// 5. 你应该有一个名为 fifthResult  的变量，它正确地将两个数字相乘以得到乘积 20 。将表达式中当前使用的字串替换为数字。
+const fifthResult = 20;
+console.log(`10 * 2 = ${fifthResult}`);
+
+// 6. 你应该有一个名为 sixthResult  的变量，它正确地添加两个数字以产生值 22 。将表达式中当前使用的 null  替换为一个数字。
+const sixthResult = 22;
+console.log(`0 + 22 = ${sixthResult}`);
