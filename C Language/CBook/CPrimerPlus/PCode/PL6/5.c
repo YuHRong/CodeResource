14147700a5acd614423ab1f2d5c08ae454350d32
YuHRong <1791867903@qq.com>
2026-03-05 10:10:58 +0800

    修改《C Primer Plus》 第六章编程练习第5题。

--- C Language/CBook/CPrimerPlus/PCode/PL6/5.c
+++ C Language/CBook/CPrimerPlus/PCode/PL6/5.c
@@ -1,34 +1,46 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#include <stdio.h>
 
-int main(void)
-{
- int i = 0;
- char ch;
+int main() {
+ char input_char;
+ int num_rows;
 
+ // 提示用户输入大写字母
  printf("请输入一个大写字母: ");
- scanf("%c", &ch);
+ scanf(" %c", &input_char);
 
- while (i++ < 5)
- {
-  int j = 1, k = 5;
-  int c = ch;
-  while (k > c)
-  {
-   putchar(' ');    // 打印空格
-   k--;
+ // 验证用户输入是否为大写字母
+ //if (input_char < 'A' || input_char > 'Z') {
+  //printf("输入无效。请输入一个大写字母。\n");
+  //return 1; // 表示程序出错退出
+ //}
+
+ // 计算行数，根据输入字母决定
+ num_rows = input_char - 'A' + 1;
+
+ // 外层循环处理行
+ for (int i = 1; i <= num_rows; i++) {
+  // 内层循环1：打印空格
+  // 每行需要打印的空格数是 num_rows - i
+  for (int j = 0; j < num_rows - i; j++) {
+   printf(" ");
   }
-  while (j < i)
-  {
-   printf("%c", c++);
-   j++;
+
+  // 内层循环2：以升序打印字母
+  // 从 'A' 开始，打印到当前行对应的字母
+  for (int k = 0; k < i; k++) {
+   printf("%c", 'A' + k);
   }
-  while (j > 0)
-  {
-   printf("%c", c--);
-   j--;
+
+  // 内层循环3：以降序打印字母
+  // 从当前行倒数第二个字母开始，打印到 'A'
+  for (int l = i - 2; l >= 0; l--) {
+   printf("%c", 'A' + l);
   }
+
+  // 打印完一行后换行
   printf("\n");
  }
+
  return 0;
 }
