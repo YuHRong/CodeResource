c748f580c614df16b5d0ead931e11e398cf2c95f
YuHRong <1791867903@qq.com>
2026-03-16 09:49:15 +0800

    添加 BinaryWriter 和 BinaryReader 写入读取 二进制文件学习

--- C#/Study/File/LP5SampleApps/Files_M1/Starter/Program.cs
+++ C#/Study/File/LP5SampleApps/Files_M1/Starter/Program.cs
@@ -263,9 +263,75 @@ class Program
             foreach (var transaction in account.Transactions)
             {
                 // Append transaction data to the StringBuilder object
-                sb.AppendLine($"{transaction.TransactionId},{transaction.TransactionType},{transaction.TransactionDate},{transaction.TransactionTime},{transaction.PriorBalance:F2},{transaction.TransactionAmount:F2},{transaction.SourceAccountNumber},{transaction.TargetAccountNumber},{transaction.Description}");
+                sb.AppendLine($"{transaction.TransactionId},{transaction.TransactionType},{transaction.TransactionDate},{transaction.TransactionTime},{transaction.PriorBalance:F2},{transaction.TransactionAmount:f2},{transaction.SourceAccountNumber},{transaction.TargetAccountNumber},{transaction.Description}");
             }
         }
+
         Console.WriteLine($"\n\nUse the FileStream class to perform file I/O operations.");
+
+        //使用FileStream将事务数据写入文件
+        using (FileStream fileStream = new FileStream(fileStreamPath, FileMode.Create, FileAccess.Write))
+        {
+            // Convert the StringBuilder object to a byte array and write the byte array to the file
+            byte[] transactionDataBytes = new UTF8Encoding(true).GetBytes(sb.ToString());
+
+            // Use the Write method to write the byte array to the file
+            fileStream.Write(transactionDataBytes, 0, transactionDataBytes.Length);
+            Console.WriteLine($"\nFile length after write: {fileStream.Length} bytes");
+
+            // Use the Flush method to ensure all data is written to the file
+            fileStream.Flush();
+        }
+
+        Console.WriteLine($"\nTransaction data written using FileStream. File: {fileStreamPath}");
+
+        using (FileStream fileStream = new FileStream(fileStreamPath, FileMode.Open, FileAccess.Read))
+        {
+            byte[] readBuffer = new byte[1024];
+            UTF8Encoding utf8Decoder = new UTF8Encoding(true);
+            int bytesRead;
+
+            Console.WriteLine("\nUsing FileStream to read/display transaction data.\n");
+
+            while ((bytesRead = fileStream.Read(readBuffer, 0, readBuffer.Length)) > 0)
+            {
+                Console.WriteLine($"bytes read: {utf8Decoder.GetString(readBuffer, 0, bytesRead)}\n");
+            }
+
+
+            Console.WriteLine($"File length: {fileStream.Length} bytes");
+            Console.WriteLine($"Current position: {fileStream.Position} bytes");
+
+            // Use the Seek method to move to the beginning of the file
+            fileStream.Seek(0, SeekOrigin.Begin);
+            Console.WriteLine($"Position after seek: {fileStream.Position} bytes");
+
+            // Use the FileStream.Read method to read the first 100 bytes
+            bytesRead = fileStream.Read(readBuffer, 0, 100);
+            Console.WriteLine($"Read first 100 bytes: {utf8Decoder.GetString(readBuffer, 0, bytesRead)}");
+
+        }
+
+        // Create a filepath for a binary file named binary.dat
+        string binaryFilePath = Path.Combine(currentDirectory, "binary.dat");
+
+        // Create a BinaryWriter object and write binary data to the binary.dat file
+        using (BinaryWriter binaryWriter = new BinaryWriter(File.Open(binaryFilePath, FileMode.Create)))
+        {
+            binaryWriter.Write(1.25);
+            binaryWriter.Write("Hello");
+            binaryWriter.Write(true);
+        }
+
+        Console.WriteLine($"\n\nBinary data written to: {binaryFilePath}");
+
+        // Create a BinaryReader object and read binary data from the binary.dat file
+        using (BinaryReader binaryReader = new BinaryReader(File.Open(binaryFilePath, FileMode.Open)))
+        {
+            double a = binaryReader.ReadDouble();
+            string b = binaryReader.ReadString();
+            bool c = binaryReader.ReadBoolean();
+            Console.WriteLine($"Binary data read from {binaryFilePath}: {a}, {b}, {c}");
+        }
     }
 }
