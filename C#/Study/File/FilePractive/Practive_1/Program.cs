fc99ac279a91a90c5c9b5b174f6697ea2d220598
YuHRong <1791867903@qq.com>
2026-03-19 08:47:27 +0800

    添加文件写入读取练习

---
+++ C#/Study/File/FilePractive/Practive_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+ static void Main()
+ {
+  string directoryPath = @"E:example";
+  string fileName = "Example.txt";
+  string filePath = Path.Combine(directoryPath, fileName);
+
+  // 创建新目录
+  Directory.CreateDirectory(directoryPath);
+
+  // 创建一个新文件并写入文本
+  File.WriteAllText(filePath, "练习文本。\nhello world!\n你好世界！");
+
+  // 读取文本
+  string text = File.ReadAllText(filePath);
+  Console.WriteLine(text);
+ }
+}
