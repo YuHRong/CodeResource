304e4e299299e580b5d1010224ba6c38e6e0ecb2
YuHRong <1791867903@qq.com>
2026-03-23 00:32:54 +0800

    添加接口学习，添加实现接口练习

---
+++ C#/Study/实现接口/发现接口/TrainingProjects/ImplementInterfaces/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ImplementInterfaces
+{
+ class Program
+ {
+  static void Main(string[] arg)
+  {
+   IPerson student = new Student { Name = "Eric Solomon", Age = 20 };
+   IPerson teacher = new Teacher { Name = "Kayla Lewis", Age = 35 };
+
+   student.DisplayInfo();
+   teacher.DisplayInfo();
+  }
+ }
+}
