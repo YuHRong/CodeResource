49fc2e3fb477ce9ae141bdedea89a75ccf96e93d
YuHRong <1791867903@qq.com>
2026-04-23 18:24:11 +0800

    新增 StaticClass 项目及静态类演示代码
    新增 StaticClass 控制台项目，包含 UserMessage 静态类及 ApplicationStartMessage 方法，实现根据当前时间输出问候语，并已配置为 .NET 8，支持隐式 using 和可空引用类型。同步更新解决方案文件以包含该项目。

---
+++ C#/Study/C#大师班/ClassCode/StaticClass/Program.cs
@@ -0,0 +1,4 @@
+﻿
+using StaticClass;
+
+UserMessage.ApplicationStartMessage();
---
+++ C#/Study/C#大师班/ClassCode/StaticClass/UserMessage.cs
@@ -0,0 +1,23 @@
+﻿
+namespace StaticClass;
+
+public class UserMessage
+{
+ public static void ApplicationStartMessage()
+ {
+  Console.WriteLine("欢迎使用 Static 静态类演示应用程序！");
+
+  Console.ReadLine();
+  Console.Clear();
+
+  int hoursDay = DateTime.Now.Hour;
+  //hoursDay = 22;
+  Console.WriteLine(hoursDay);
+  if (hoursDay < 12)
+   Console.WriteLine("Good Morning!");
+  else if (hoursDay < 19)
+   Console.WriteLine("Good Afternoon!");
+  else
+   Console.WriteLine("Good Evening!");
+ }
+}
