01412dd9b971047a2f8b316a0dfbee1238f34c11
YuHRong <1791867903@qq.com>
2026-04-23 12:05:24 +0800

    添加一个综合作业（待完成）。

---
+++ C#/Study/C#大师班/PracticeCode/Homework2/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("欢迎使用宾客信息登记表");
+Console.WriteLine("***********************************************");
+Console.WriteLine();
+
+Console.Write("请问您的姓名是： ");
+Console.ReadLine();
+Console.WriteLine("您同行人数是？");
+Console.ReadLine();
+
