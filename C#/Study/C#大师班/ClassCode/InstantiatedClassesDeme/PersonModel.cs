24872e71981ee7c7c8c53f6c0280d515ecdffbe5
YuHRong <1791867903@qq.com>
2026-05-09 10:32:48 +0800

    新增第14章C语言结构，及C#类实例化演示项目
    为CPrimerPlus添加L14项目及相关C代码示例（book.c, manybook.c），并引入VS工程文件。为C#大师班解决方案新增InstantiatedClassesDeme项目，包含PersonModel类及其使用示例。完善Note.md文档，丰富OOP与结构化编程内容。同步子模块状态。

---
+++ C#/Study/C#大师班/ClassCode/InstantiatedClassesDeme/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿
+namespace InstantiatedClassesDeme;
+
+public class PersonModel
+{
+//public string LastName;
+
+ //public string FirstName;
+
+//public string EmailAddress;
+
+ public int MyProperty { get; set; }
+ public string FirstName { get; set; }
+public string LastName { get; set; }
+
+}
---
+++ C#/Study/C#大师班/ClassCode/InstantiatedClassesDeme/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using InstantiatedClassesDeme;
+
+
+
+//List<PersonModel> people = new List<PersonModel>();
+//PersonModel person = new PersonModel();
+//person.FirstName = "Tim";
+//people.Add(person);
+//person = new PersonModel();
+//Console.WriteLine(person.LastName);
+//Console.WriteLine(person.FirstName);
+
+List<PersonModel> people = new List<PersonModel>();
+foreach(var p in people)
+{
+ Console.WriteLine(p.FirstName);
+}
