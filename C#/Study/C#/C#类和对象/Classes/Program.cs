7a96693cb08b61732657f7a5110c7b87932b924d
YuHRong <1791867903@qq.com>
2026-05-21 11:51:10 +0800

    添加类和对象练习。

---
+++ C#/Study/C#/C#类和对象/Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Classes;
+
+var account = new BankAccount("{tom}", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
+
+// 测试初始余额必须为证
+// BankAccount invalidAccount;/* 
+// try
+// {
+// invalidAccount = new BankAccount("invalid", -55);
+// }
+// catch(ArgumentOutOfRangeException e)
+// {
+     // Console.WriteLine("Exception caught creating account with negative balance");
+         // Console.WriteLine(e.ToString());
+             // return;
+// }
+
+Console.WriteLine(account.GetAccountHistory());
