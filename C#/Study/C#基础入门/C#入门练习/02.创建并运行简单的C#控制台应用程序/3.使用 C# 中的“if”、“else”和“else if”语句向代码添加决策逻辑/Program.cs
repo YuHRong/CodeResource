c8877e227c8cc3f3f60b20d9eb7ccfadf8cc9147
YuHRong <1791867903@qq.com>
2026-06-03 00:55:38 +0800

    添加C#入门第二章创建并运行简单的C#控制台程序

---
+++ C#/Study/C#基础入门/C#入门练习/02.创建并运行简单的C#控制台应用程序/3.使用 C# 中的“if”、“else”和“else if”语句向代码添加决策逻辑/Program.cs
@@ -0,0 +1,33 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+
+// 如果用户的订阅已过期
+ if (daysUntilExpiration == 0)
+{
+ Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+ Console.WriteLine("Your subscription expires within a day!");
+ discountPercentage = 20;
+}
+// 如果用户的订阅将在 5 天或更短的时间内到期
+else if (daysUntilExpiration <= 5)
+{
+ Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+ discountPercentage = 10;
+}
+// 如果用户的订阅将在 10 天或更短的时间内到期
+else if (daysUntilExpiration <= 10)
+{
+ Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+// 创建一个单独的  if  语句来显示折扣套餐。
+if (discountPercentage > 0)
+{
+ Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
