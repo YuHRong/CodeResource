4a8ff16a1d3bb35013bee219b2e045d702588b57
YuHRong <1791867903@qq.com>
2026-06-02 23:02:43 +0800

    更新项目文件和添加收银机模拟应用
    更新 `.gitignore` 文件以忽略构建结果和用户特定文件。
    添加 `LICENSE` 文件，声明使用 MIT 许可证。
    添加 `README.md` 文件，描述项目用途。
    在 `Program.cs` 中实现收银机模拟应用，支持交易管理、找零逻辑和异常处理。
    修复 `MakeChange` 方法中的逻辑错误，确保找零正确。
    添加字符串处理示例，包括单词反转和订单解析。
    添加压缩文件 `Guided-project-debugging-CSharp-main.zip`，包含起始和最终代码。

---
+++ C#/Study/C#基础入门/C#入门练习/01.使用C#编写第一个代码/2.使用C#中的文本值和变量值存储和检索数据/Program.cs
@@ -0,0 +1,6 @@
+﻿
+string name = "Bob";
+int msg = 3;
+decimal temp = 34.4m;
+
+Console.WriteLine($"Hello, {name}! You have {msg} messages in your inbox. The temperature is {temp} celsius.");
---
+++ C#/Study/C#基础入门/C#入门练习/01.使用C#编写第一个代码/4.对C#中的数字执行基本操作/Program.cs
@@ -0,0 +1,6 @@
+﻿// 将华氏度转换为摄氏度
+
+int fahrenheit = 94;
+decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+
+Console.WriteLine($"The temperature is {celsius:F1} Celsius.");
