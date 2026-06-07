c110395357ee79ad168d3f991c5fb091fc3a8405
YuHRong <1791867903@qq.com>
2026-06-07 22:44:19 +0800

    新增多个C#示例及项目配置更新
    更新详情：
    - 修改 `.gitignore` 文件，新增忽略规则（如 `.zip`、`**/bin/` 等）。
    - 更新 `C#高级编程-10.sln`，新增多个项目引用及配置。
    - 删除 `第3张.csproj` 文件内容。
    - 更新子模块 `mslearn-csharp-null-safety` 状态。
    - 新增多个 C# 示例代码文件（如 `HelloWorld.cs` 等）。
    - 更新 `Readme.md`，新增第 1 章和第 2 章代码说明。
    - 新增代码示例，涵盖命令行参数、枚举、循环、条件语句、变量作用域等主题。
    - 新增多个 `App.config` 和 `AssemblyInfo.cs` 文件，配置运行时及程序集元信息。
    - 新增数学库 `Calculator.cs`，实现加法和减法功能。
    - 简化部分代码，使用 `using static System.Console`。

---
+++ C#/C#Book/C#高级编程-10/BCode/Ch02/VariableScopeSample2/Program.cs
@@ -0,0 +1,7 @@
+﻿int j = 20;
+for (int i = 0; i < 10; i++)
+{
+ int j = 30; // Can't do this - j is still in scope
+ WriteLine(j + i);
+}
+return 0;
---
+++ C#/C#Book/C#高级编程-10/BCode/Ch02/VariableScopeSample3/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+
+class Program
+{
+ static int j = 20;
+ static void Main()
+ {  int j = 30;
+  WriteLine(j);
+  WriteLine(Program.j);
+  return;
+ }
+}
+
