f396b781461c2270e1afd2f40f17746115d537fe
YuHRong <1791867903@qq.com>
2026-06-13 00:07:57 +0800

    添加判断数字为几位数的 C语言实力。

---
+++ C Language/CPractice/c语言实力/c语言实力/28_判断数字为几位数.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void) {
+ long long n;
+ int count = 0;
+
+ printf("输入一个整数: ");
+ scanf("%lld", &n);
+
+ while (n != 0) {
+  // n = n/10
+  n
+   /= 10;
+  count++;
+ }
+ printf("数字是 %d 位数。", count);
+
+ return 0;
+}
