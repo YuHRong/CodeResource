622b911f4534284e616740b1c8f9559522eda649
YuHRong <1791867903@qq.com>
2026-06-12 23:58:35 +0800

    Good Night

---
+++ C#/Study/C#基础入门/C#入门练习/05.在C#控制台应用程序中创建方法/1.编写你的第一个C#方法/Class/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+if ipAddress consists of 4 numbers
+and
+if each ipAddress number has no leading zeroes
+and
+if each ipAddress number is in range 0 - 255
+
+then ipAddress is valid
+
+else ipAddress is invalid
+*/
+
+if(ValidateLength() && ValidateZeroes() && ValidateRange())
+{
+     Console.WriteLine($"ip is a valid IPv4 address");
+}
+else
+{
+     Console.WriteLine($"ip is an invalid IPv4 address");
+}
+
+void ValidateLength() {}
+void ValidateZeroes() {}
+void ValidateRange() {}
