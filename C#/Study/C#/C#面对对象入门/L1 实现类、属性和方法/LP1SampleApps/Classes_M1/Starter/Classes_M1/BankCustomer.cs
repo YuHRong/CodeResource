ccd453c1c5a4ac642a2cd30e6cb3a95fa524e270
YuHRong <1791867903@qq.com>
2026-06-19 22:05:24 +0800

    添加创建 C# 类练习

---
+++ C#/Study/C#/C#面对对象入门/L1 实现类、属性和方法/LP1SampleApps/Classes_M1/Starter/Classes_M1/BankCustomer.cs
@@ -0,0 +1,24 @@
+﻿namespace Classes_M1;
+
+public class BankCustomer
+{
+ private static int s_nextCustomerId;
+ public string FirstName = "Tim";
+ public string LastName = "Shao";
+ public readonly string CustomerId;
+
+ static BankCustomer()
+ {
+  Random random = new Random();
+  s_nextCustomerId = random.Next(10000000, 20000000);
+ }
+
+ public BankCustomer(string firstName, string lastName)
+ {
+  firstName = FirstName;
+  lastName = LastName;
+  this.CustomerId = (s_nextCustomerId++).ToString("D10");
+ }
+
+}
+
