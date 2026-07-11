ad25c1716cee051606b00ab4301442fae1b18a95
YuHRong <1791867903@qq.com>
2026-07-12 06:57:28 +0800

    添加 w3resource c++ 基础练习

---
+++ C++/Studies/w3resource/basicExercise/1.Welcome Text on Separate Line.cpp
@@ -0,0 +1,10 @@
+// Write a program in C++ to print welcome text on a separate line.
+
+#include <iostream>
+
+int main()
+{
+ std::cout << "Hello cpp!" << std::endl;
+
+ return 0;
+}
---
+++ C++/Studies/w3resource/basicExercise/4.Sum Using Variables.cpp
@@ -0,0 +1,21 @@
+// Write a program in C++ to print the sum of two numbers using variables.
+#include<iostream>
+
+using namespace std;
+
+int main(){
+ int num1, num2;
+
+ cout << "Input first number: ";
+ cin >> num1;
+ cout << "Input seccond number: ";
+ cin >> num2;
+
+cout << "Print the sum of two numbers :" << endl;
+
+cout << "-----------------------------------" << endl;
+
+cout << "The sum of " << num1 << " and " << num2 << " is " << " : " << num1 + num2 << endl;
+
+return 0;
+}
