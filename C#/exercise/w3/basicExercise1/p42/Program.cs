d4a25fbe275ddd965efea933a32667311e9e2d49
YuHRong <1791867903@qq.com>
2026-07-16 13:10:13 +0800

    添加两个练习

---
+++ C#/exercise/w3/basicExercise1/p42/Program.cs
@@ -0,0 +1,15 @@
+﻿// Write a C# program to create a string where the first 4 characters are in lower case. If the string is less than 4 letters, make the whole string in upper case.
+
+string str;
+
+
+Console.Write("Input a string : ");
+str = Console.ReadLine();
+
+if (str.Length >= 4)
+{
+ // 如果字符串长度大于4，则将前四个字符转换为大写
+ // 并将字符串剩余部分添加并打印
+ Console.WriteLine(str.Substring(0, 4).ToUpper() + str.Substring(4, str.Length - 4));
+}
+else    // 若字符串长度小于 4，则将全部字符转换为大写 Console.WriteLine(str.ToUpper());
---
+++ C#/exercise/w3/basicExercise1/p43/Program.cs
@@ -0,0 +1,20 @@
+﻿// Write a C# program to check if a given string starts with "w" and is immediately followed by two "ww".
+
+Console.Write("Input a string : ");
+string str = Console.ReadLine();
+
+bool isW = true;
+
+for (int i = 0; i < str.Length - 1; i++)
+{
+ if (str[0] == 'w')
+ {
+  if (str.Substring(0, 3).Equals("ww"))
+   isW = true;
+  else isW = false;
+ }
+ else
+  isW = false;
+}
+
+Console.WriteLine(isW);
