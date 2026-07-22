0a81ac1b559ced8301160ebef8207bad43d5579e
YuHRong <1791867903@qq.com>
2026-07-22 15:47:14 +0800

    添加几个基础练习

---
+++ C#/exercise/w3/basicExercise1/56. Check Palindrome String/Program.cs
@@ -0,0 +1,19 @@
+﻿// Write a C# program to check if a given string is a palindrome or not.
+
+Console.WriteLine("Enter the string: "); ;
+string? str = Console.ReadLine();
+
+Console.WriteLine($"For '{str}' the output should be ");
+Console.WriteLine(CharPalindrome(str));
+
+static bool CharPalindrome(string inputStr)
+{
+ // 将字符串拆分为字符数组
+ char[] c = inputStr.ToCharArray();
+
+ // 反转字符数组
+ Array.Reverse(c);
+
+ // 检查原始字符串是否等于反转字符串
+ return new string(c).Equals(inputStr);
+}
---
+++ C#/exercise/w3/basicExercise1/p57. Max Product of Adjacent Integers/Program.cs
@@ -0,0 +1,24 @@
+﻿// Write a C# program to find the pair of adjacent elements that has the highest product of an array of integers.
+
+// 输入测试
+
+Console.WriteLine(AdjacentElementsProduct(new int[] { 1, 3, -5, -4, 8, -5 }));
+Console.WriteLine(AdjacentElementsProduct(new int[] { 1, 3, -4, 5, 2 }));
+Console.WriteLine(AdjacentElementsProduct(new int[] { 1, 3, 4, 5, 2 }));
+
+static int AdjacentElementsProduct(int[] inputArray)
+{
+ // 用数组中前两个元素的乘积初始化最大乘积
+ int max = inputArray[0] * inputArray[1];
+
+
+ // 遍历数组以找到相邻元素的最大乘积
+ for (int i = 1; i < inputArray.Length - 2; i++)
+ {
+  // 用当前最大值和当前元素与下一个元素的乘积的最大值更新max变量
+  max = Math.Max(max, inputArray[i] * inputArray[i + 1]);
+ }
+
+ // 返回相邻元素的最大乘积
+ return max;
+}
