48e6d1213d816fac713f2b5fed664256cc0a45dd
YuHRong <1791867903@qq.com>
2026-07-25 10:29:40 +0800

    添加 C# 继承、枚举

---
+++ C#/exercise/class/Inherit/e1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+// 基类
+class Shape
+{
+
+ public void SetWidth(int w)
+ {
+  width = w;
+ }
+
+ public void SetHeight(int h)
+ {
+  height = h;
+ }
+
+ protected int width;
+ protected int height;
+}
+
+// 派生类
+class Rectangle : Shape
+{
+ public int GetArea()
+ {
+  return (width * height);
+ }
+}
+
+class RectangleTester
+{
+ static void Main()
+ {
+  Rectangle rect = new Rectangle();
+  rect.SetWidth(5);
+  rect.SetHeight(7);
+
+  Console.WriteLine($"总面积 {rect.GetArea()}");
+  Console.ReadKey();
+ }
+}
