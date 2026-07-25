087273cefa7551c37e66a051c24d6a1b16ea0133
YuHRong <1791867903@qq.com>
2026-07-25 16:40:17 +0800

    添加 NAudio 学习

---
+++ C#/Study/audio/NAudio/AudioApp/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AudioApp
+{
+ partial class Form1
+ {
+  /// <summary>
+  /// 必需的设计器变量。
+  /// </summary>
+  private System.ComponentModel.IContainer components = null;
+
+  /// <summary>
+  /// 清理所有正在使用的资源。
+  /// </summary>
+  /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+  protected override void Dispose(bool disposing)
+  {
+   if (disposing && (components != null))
+   {
+    components.Dispose();
+   }
+   base.Dispose(disposing);
+  }
+
+  #region Windows 窗体设计器生成的代码
+
+  /// <summary>
+  /// 设计器支持所需的方法 - 不要修改
+  /// 使用代码编辑器修改此方法的内容。
+  /// </summary>
+  private void InitializeComponent()
+  {
+   this.OnButtonPlay = new System.Windows.Forms.Button();
+   this.OnButtonStop = new System.Windows.Forms.Button();
+   this.SuspendLayout();
+   // 
+   // OnButtonPlay
+   // 
+   this.OnButtonPlay.Location = new System.Drawing.Point(0, 2);
+   this.OnButtonPlay.Name = "OnButtonPlay";
+   this.OnButtonPlay.Size = new System.Drawing.Size(75, 23);
+   this.OnButtonPlay.TabIndex = 0;
+   this.OnButtonPlay.Text = "buttonPlay";
+   this.OnButtonPlay.UseVisualStyleBackColor = true;
+   this.OnButtonPlay.Click += new System.EventHandler(this.OnButtonPlay_Click);
+   // 
+   // OnButtonStop
+   // 
+   this.OnButtonStop.Location = new System.Drawing.Point(8, 8);
+   this.OnButtonStop.Name = "OnButtonStop";
+   this.OnButtonStop.Size = new System.Drawing.Size(75, 23);
+   this.OnButtonStop.TabIndex = 1;
+   this.OnButtonStop.Text = "buttonStop";
+   this.OnButtonStop.UseVisualStyleBackColor = true;
+   this.OnButtonStop.Click += new System.EventHandler(this.OnButtonStop_Click);
+   // 
+   // Form1
+   // 
+   this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+   this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+   this.ClientSize = new System.Drawing.Size(800, 450);
+   this.Controls.Add(this.OnButtonStop);
+   this.Controls.Add(this.OnButtonPlay);
+   this.Name = "Form1";
+   this.Text = "Form1";
+   this.ResumeLayout(false);
+
+  }
+
+  #endregion
+
+  private System.Windows.Forms.Button OnButtonPlay;
+  private System.Windows.Forms.Button OnButtonStop;
+ }
+}
+
---
+++ C#/Study/audio/NAudio/AudioApp/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace AudioApp
+{
+ public partial class Form1 : Form
+ {
+  private WaveOut outputDevice;
+  private AudioFileReader audioFile;
+
+  public Form1()
+  {
+   InitializeComponent();
+  }
+
+  private void OnPlaybackStopped(object sender, StoppedEventArgs args)
+  {
+   outputDevice.Dispose();
+   outputDevice = null;
+   audioFile.Dispose();
+   audioFile = null;
+  }
+
+  private void OnButtonStop_Click(object sender, System.EventArgs e)
+  {
+   outputDevice?.Stop();
+  }
+
+  private void OnButtonPlay_Click(object sender, System.EventArgs e)
+  {
+   if (outputDevice == null)
+   {
+    outputDevice = new WaveOut();
+    outputDevice.PlaybackStopped += OnPlaybackStopped;
+   }
+   if (audioFile == null)
+   {
+    audioFile = new AudioFileReader(@"E:\example.mp3");
+    outputDevice.Init(audioFile);
+   }
+   outputDevice.Play();
+  }
+ }
+}
