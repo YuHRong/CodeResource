753764945f14aa9ed870a091f936fcf76c8cc888
YuHRong <1791867903@qq.com>
2026-07-26 16:19:54 +0800

    添加一个冒泡排序算法。 添加一个基础练习

---
+++ C#/exercise/w3/arrayExercise/冒泡排序/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] arr1 = { 3, 4, 2, 5, 7, 1, 9 };
+int n = arr1.Length;
+
+for (int i = 0; i < n - 1; i++)
+{
+ for (int j = 0; j < n - 1 - i; j++)
+ {
+  if (arr1[j] > arr1[j + 1])
+  {
+   int temp = arr1[j];
+   arr1[j] = arr1[j + 1];
+   arr1[j + 1] = temp;
+  }
+ }
+}
+
+for (int i = 0; i < n; i++)
+{
+ Console.Write(arr1 + " ");
+}
+
+Console.WriteLine();
+
+Console.ReadKey();
---
+++ C#/exercise/w3/basicExercise1/p59. Check Strictly Increasing Sequence/Program.cs
@@ -0,0 +1,27 @@
+﻿// Write a C# program to check whether it is possible to create a strictly increasing sequence from a given sequence of integers as an array.
+
+bool TestIncreasingSequence(int[] int_seq)
+{
+ int count = 0;    //计数器用于跟踪所需更改的数量
+
+ // 遍历数组以检查非递增元素并计算所需的更改
+ for(int i = 0; i < int_seq.Length - 1; i++)
+ {
+  // 检查当前元素是否大于或等于下一个元素
+  if (int_seq[i] >= int_seq[i + 1])
+   count++;   // 需要更改时增加计数器
+
+  // 通过比较当前和下一个元素检查是否可能更大
+  if (i + 2 < int_seq.Length && int_seq[i] >= int_seq[i + 2])
+   count++;    // 需要更改时增加计数器
+ }
+
+ // 如果所需更改的数量最多为2（即数组最多可以增加一次更改），则返回true
+ return count <= 2;
+}
+
+// 测试数据
+Console.WriteLine(TestIncreasingSequence(new int[] { 1, 3, 5, 6, 9 }));  // Output: True
+Console.WriteLine(TestIncreasingSequence(new int[] { 0, 10 }));         // Output: True
+Console.WriteLine(TestIncreasingSequence(new int[] { 1, 3, 1, 3 }));    // Output: False
+
