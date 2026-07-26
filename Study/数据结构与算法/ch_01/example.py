c55b43995c00ee832ce67a1075cd5a39e7591b6f
YuHRong <1791867903@qq.com>
2026-07-26 14:08:00 +0800

    添加数据结构与算法示例

---
+++ Study/数据结构与算法/ch_01/example.py
@@ -0,0 +1,3 @@
+array = {"apples", "bananas", "cucumbers", "dates", "elderberries"}
+
+print(type(array))
---
+++ Study/数据结构与算法/ch_03/example.py
@@ -0,0 +1,6 @@
+# 打印列表所有元素
+
+things = ['apples', 'baboons', 'cribs', 'dulcimers'] 
+
+for thing in things:
+    print(thing)
