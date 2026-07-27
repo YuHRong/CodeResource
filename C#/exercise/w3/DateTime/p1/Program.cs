32967a077da6f7712f652a665f762d40928fdd80
YuHRong <1791867903@qq.com>
2026-07-27 17:58:34 +0800

    添加 C# DateTime 练习

---
+++ C#/exercise/w3/DateTime/p1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Write a C# Sharp program to extract the Date property and display the DateTime value in the formatted output.
+
+// 创建具有特定时间值的 DateTime 对象
+DateTime dn = new DateTime(2016, 6, 8, 11, 49, 0);
+// 从 DateTime 中获取日期不带时间
+DateTime dateOnly = dn.Date;
+
+Console.WriteLine("Complete date: " + dn.ToString("M/d/yyyy h:mm:ss tt"));
+
+// 使用 d 格式说明符打印日期部分
+Console.WriteLine("Short Date: " + dateOnly.ToString("M/d/yyyy"));
+
+// 使用24小时制显示
+Console.WriteLine("Display date using 24-hour clock format:");
+
+// 使用 g 格式说明符打印短日期和时间
+Console.WriteLine(dateOnly.ToString("M/d/yyyy h:mm tt"));
+
+// 以 24 小时制以自定义格式显示日期和时间
+Console.WriteLine(dateOnly.ToString("MM/dd/yyyy HH:mm"));
---
+++ C#/exercise/w3/DateTime/p2/Program.cs
@@ -0,0 +1,10 @@
+﻿
+DateTime dayNow = new DateTime(2016, 8, 16, 3, 57, 32, 11);
+
+Console.WriteLine("Year = " + dayNow.Year);
+Console.WriteLine("Month = " + dayNow.Month);
+Console.WriteLine("day = " + dayNow.Day);
+Console.WriteLine("hour = " + dayNow.Hour);
+Console.WriteLine("minute = " + dayNow.Minute);
+Console.WriteLine("second = " + dayNow.Second);
+Console.WriteLine("millisecond = "  + dayNow.Millisecond);
