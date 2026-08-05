b3f0fce1f18984540634794c5f1bc660b03c4f91
YuHRong <1791867903@qq.com>
2026-08-05 19:52:24 +0800

    添加 C# 异常处理练习

---
+++ C#/exercise/w3/ExceptionHandling/p1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Write a C# program that prompts the user to input two numbers and divides them. Handle an exception when the user enters non-numeric values.
+
+namespace ExceptionHandling;
+
+public class Program
+{
+ static void Main(string[] args)
+ {
+  try
+  {
+   // 提示输入第一个数字
+   Console.WriteLine("Input first number: "); ;
+   string input1 = Console.ReadLine();    // 接受字符串输入
+   double num1 = Convert.ToDouble(input1);    // 将输入转换为 double 类型
+
+   // 提示输入第二个数字
+   Console.WriteLine("Input second number: ");
+   string input2 = Console.ReadLine();    // 接受字符串输入
+   double num2 = Convert.ToDouble(input2);    // 将字符串转换为 double 类型
+
+   // 检查 被除数是否为 0
+   if (num2 != 0)
+   {
+    Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+   }
+   else
+   {
+    Console.WriteLine("Error: Cannot divide by zero.");
+   }
+  }
+  catch (FormatException)
+  {
+   // 用于处理非数字输入错误的捕获
+   Console.WriteLine("Error: Non-numeric value entered.");
+  }
+  catch (Exception ex)
+  {
+   // 用于处理其他类型异常的捕获
+   Console.WriteLine("An error occurred: " + ex.Message);
+  }
+ }
+}
---
+++ C#/exercise/w3/ExceptionHandling/p2/Program.cs
@@ -0,0 +1,46 @@
+﻿//Write a C# program to implement a method that takes an integer as input and throws an exception if the number is negative. Handle the exception in the calling code.
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+try
+{
+ Console.WriteLine("Input an integer: ");
+ string input = Console.ReadLine();
+ int n = Convert.ToInt32(input);
+
+ IntegerNum(n);
+
+ //如果输入通过验证，则显示为有效
+ Console.WriteLine("Valid input: " + n);
+}
+catch (NegativeNumberException ex)
+{
+ // 用于处理NegativeNumberException的捕获块
+ Console.WriteLine("Error!" + ex.Message);
+}
+catch (FormatException)
+{
+ //用于处理FormatException（非整数输入）的捕获
+ Console.WriteLine("Error: Invalid input. Please enter an integer.");
+}
+catch (Exception ex)
+{
+ // Catch block for handling other types of exceptions
+ Console.WriteLine("An error occurred: " + ex.Message);
+}
+
+int IntegerNum(int n)
+{
+ if (n < 0)
+  // 如果数字为负数，则抛出NegativeNumberException
+  throw new NegativeNumberException("Negative number not allowed.");
+
+ // 如果输入的数字大于等于 0，正常返回
+ return n;
+}
+
+//用于处理负数情况的自定义异常类
+class NegativeNumberException : Exception
+{
+ public NegativeNumberException(string message) : base(message) { }
+}
