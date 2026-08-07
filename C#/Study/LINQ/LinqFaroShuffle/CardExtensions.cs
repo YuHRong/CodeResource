bea7cc5816a0ddfa5b46d7c951cc7010a81611c2
YuHRong <1791867903@qq.com>
2026-08-07 10:44:05 +0800

    添加一个 LINQ 学习实力

---
+++ C#/Study/LINQ/LinqFaroShuffle/CardExtensions.cs
@@ -0,0 +1,48 @@
+﻿namespace LinqFaroShuffle;
+
+public static class CardExtensions
+{
+ extension<T>(IEnumerable<T> sequence)
+ {
+  public IEnumerable<T> InterleaveSequenceWith(IEnumerable<T> second)
+  {
+   var firstIter = sequence.GetEnumerator();
+   var secondIter = second.GetEnumerator();
+
+   while (firstIter.MoveNext() && secondIter.MoveNext())
+   {
+    yield return firstIter.Current;
+    yield return secondIter.Current;
+   }
+
+  }
+
+  public bool SequenceEquals(IEnumerable<T> second)
+  {
+   var firstIter = sequence.GetEnumerator();
+   var secondIter = second.GetEnumerator();
+
+   while ((firstIter?.MoveNext() == true) && secondIter.MoveNext())
+   {
+    if ((firstIter.Current is not null) && !firstIter.Current.Equals(secondIter.Current))
+    {
+     return false;
+    }
+   }
+
+   return true;
+  }
+
+  public IEnumerable<T> LogQuery(string tag)
+  {
+   // File.AppendText creates a new file if the file doesn't exist.
+   using (var writer = File.AppendText("debug.log"))
+   {
+    writer.WriteLine($"Executing Query {tag}");
+   }
+
+   return sequence;
+  }
+
+ }
+}
---
+++ C#/Study/LINQ/LinqFaroShuffle/Program.cs
@@ -0,0 +1,129 @@
+﻿using LinqFaroShuffle;
+
+/* 
+var startingDeck = from s in Suits()
+                   from r in Ranks()
+                   select (Suit: s, Rank: r);
+ 
+ 
+ // var stardingDeck = Suits().SelectMany(suit => Ranks().Select(rank => (Suit: suit, Rank: rank)));
+
+// 显示生成并放入 StardingDeck 的每张卡
+foreach (var card in startingDeck)
+{
+ Console.WriteLine(card);
+}
+
+var top = startingDeck.Take(26);
+var bottom = startingDeck.Skip(26);
+
+var shuffledDeck = top.InterleaveSequenceWith(bottom);
+
+var times = 0;
+// Re-use the shuffle variable from earlier, or you can make a new one
+shuffledDeck = startingDeck;
+do
+{
+ // Out shuffle
+ 
+ shuffle = shuffle.Take(26)
+         .LogQuery("Top Half")
+        .InterleaveSequenceWith(shuffle.Skip(26)
+                .LogQuery("Bottom Half"))
+                        .LogQuery("Shuffle");
+
+
+ // shuffledDeck = shuffledDeck.Take(26).InterleaveSequenceWith(shuffledDeck.Skip(26));
+ // shuffledDeck = shuffledDeck.Skip(26).InterleaveSequenceWith(shuffledDeck.Take(26));
+
+ // In shuffle
+shuffledDeck  = shuffledDeck.Skip(26).LogQuery("Bottom Half")
+             .InterleaveSequenceWith(shuffledDeck.Take(26).LogQuery("Top Half"))
+                         .LogQuery("Shuffle");
+
+ foreach (var card in shuffledDeck)
+ {
+  Console.WriteLine(card);
+ }
+ Console.WriteLine();
+ times++;
+} while (!startingDeck.SequenceEquals(shuffledDeck));
+
+Console.WriteLine(times);
+ 
+
+
+foreach (var c in shuffledDeck)
+{
+Console.WriteLine(c);
+}
+*/
+
+var startingDeck = (from s in Suits().LogQuery("Suit Generation")
+                    from r in Ranks().LogQuery("Value Generation")
+                    select new { Suit = s, Rank = r })
+                    .LogQuery("Starting Deck")
+                    .ToArray();
+
+foreach (var c in startingDeck)
+{
+ Console.WriteLine(c);
+}
+
+Console.WriteLine();
+
+var times = 0;
+var shuffle = startingDeck;
+
+do
+{
+ /*
+ shuffle = shuffle.Take(26)
+     .LogQuery("Top Half")
+     .InterleaveSequenceWith(shuffle.Skip(26).LogQuery("Bottom Half"))
+     .LogQuery("Shuffle")
+     .ToArray();
+ */
+
+ shuffle = shuffle.Skip(26)
+     .LogQuery("Bottom Half")
+     .InterleaveSequenceWith(shuffle.Take(26).LogQuery("Top Half"))
+     .LogQuery("Shuffle")
+     .ToArray();
+
+ foreach (var c in shuffle)
+ {
+  Console.WriteLine(c);
+ }
+
+ times++;
+ Console.WriteLine(times);
+} while (!startingDeck.SequenceEquals(shuffle));
+
+Console.WriteLine(times);
+
+static IEnumerable<string> Suits()
+{
+ yield return "clubs";
+ yield return "diamonds";
+ yield return "hearts";
+ yield return "spades";
+}
+
+static IEnumerable<string> Ranks()
+{
+ yield return "two";
+ yield return "three";
+ yield return "four";
+ yield return "five";
+ yield return "six";
+ yield return "seven";
+ yield return "eight";
+ yield return "nine";
+ yield return "ten";
+ yield return "jack";
+ yield return "queen";
+ yield return "king";
+ yield return "ace";
+}
+
