b905f11234d41c92f14d6448a65a236424db2aa6
YuHRong <1791867903@qq.com>
2026-08-28 22:38:52 +0800

    添加一个 C# 数据类型练习。

---
+++ C#/exercise/w3/DataTypes/p5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Write a C# Sharp program that takes the radius of a circle as input and calculates the perimeter and area of the circle.
+
+double radius, perimeter, area; ;
+
+
+Console.Write("Input the radius of the circle :");
+radius = Convert.ToDouble(Console.ReadLine());
+
+perimeter = 2 * radius * Math.PI;
+area = Math.PI * Math.Pow(radius, 2);
+
+Console.WriteLine($"Perimeter of circle : {perimeter:F2}");
+Console.WriteLine($"Area of Circle : {area:F2}");
